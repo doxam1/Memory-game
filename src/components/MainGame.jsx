b3bf6c9ba334b9dabfcf2f36404c47885b236a1e
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-10-23 15:40:24 +0300

    style for max-width:700px

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -27,28 +27,7 @@ const MainGame = ({
       : setBestScore(bestScore);
   }
 
-  // useEffect(() => {
-  //   const fetchedImagesArray = [];
-  //   // if i want a live update of images i can use this, but i'm limited to 100 requests per hour.
-  //   async function fetchImage() {
-  //     const response = await fetch(
-  //       "https://api.giphy.com/v1/stickers/search?api_key=7vNx4XqSDBcod9JqE6DN18E4lSlchNh4&q=cute+dog&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
-  //       { mode: "cors" }
-  //     );
-  //     const images = await response.json();
-  //     for (let i = 0; i < 12; i++) {
-  //       fetchedImagesArray.push({
-  //         imageId: `${images.data[i].id}`,
-  //         imageUrl: `${images.data[i].images.original.url}`,
-  //       });
-  //     }
-  //     return fetchedImagesArray;
-  //   }
 
-  //   setImageArr(fetchImage());
-
-  //   return () => console.log("i'm exiting.");
-  // }, [imageArr]);
   useEffect(() => {
     async function fetchImage() {
       try {
